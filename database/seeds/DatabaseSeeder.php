40406b51e9b1c06e22ebace6f191d835d9b7bdb9
droogadulce <ahigareda.mayra@ciencias.unam.mx>
2020-05-02 01:47:49 -0500

    Creation of post's table and test data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        App\User::create([
+            'name' => 'Mayra',
+            'email' => 'mayra@mail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        factory(App\Post::class, 24)->create();
+
     }
 }
